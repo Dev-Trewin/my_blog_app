2a34fac55f5aeb1b5c2e0b3bea4f65d56e9fa62d
ROGER Trewin <rogertrewin.r@gmail.com>
2021-06-19 13:38:39 -0600

    navbar and landig page created

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
 import { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './Layouts/Navbar';
-import { Landing } from './Layouts/Landing';
+import Landing from './Layouts/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
 const App = () =>
+  <Router>
+    <Fragment >
+      <Navbar />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
 
-  <Fragment >
-    <Navbar />
-    <Landing />
-  </Fragment>
+
+    </Fragment>
+  </Router>
+
 
 
 
